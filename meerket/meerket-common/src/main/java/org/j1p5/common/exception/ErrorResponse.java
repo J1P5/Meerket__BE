4ad915b2dad71362368aa4baea501107a3c9e052
icecoff22 <icecoff2222@gmail.com>
2024-11-19 02:52:02 +0900

    refactor : status 코드 추가

--- meerket/meerket-common/src/main/java/org/j1p5/common/exception/ErrorResponse.java
+++ meerket/meerket-common/src/main/java/org/j1p5/common/exception/ErrorResponse.java
@@ -5,16 +5,18 @@ import lombok.Getter;
 @Getter
 public class ErrorResponse {
     private boolean isSuccess;
+    private int status;
     private String errorCode;
     private String message;
 
-    private ErrorResponse(boolean isSuccess, String errorCode, String message) {
+    private ErrorResponse(boolean isSuccess, int status, String errorCode, String message) {
         this.isSuccess = isSuccess;
+        this.status = status;
         this.errorCode = errorCode;
         this.message = message;
     }
 
-    public static ErrorResponse of(boolean isSuccess, String errorCode, String message) {
-        return new ErrorResponse(isSuccess, errorCode, message);
+    public static ErrorResponse of(boolean isSuccess, int status, String errorCode, String message) {
+        return new ErrorResponse(isSuccess, status, errorCode, message);
     }
 }
--- meerket/meerket-common/src/main/java/org/j1p5/common/exception/GlobalErrorCode.java
+++ meerket/meerket-common/src/main/java/org/j1p5/common/exception/GlobalErrorCode.java
@@ -6,18 +6,19 @@ import lombok.Getter;
 public enum GlobalErrorCode implements BaseErrorCode{
     // 에러 코드 작성
     ;
-
+    private final int status;
     private final String errorCode;
     private final String message;
 
-    GlobalErrorCode(String errorCode, String message) {
+    GlobalErrorCode(int status, String errorCode, String message) {
+        this.status = status;
         this.errorCode = errorCode;
         this.message = message;
     }
 
     @Override
     public ErrorResponse getErrorResponse() {
-        return ErrorResponse.of(false, errorCode, message);
+        return ErrorResponse.of(false, status, errorCode, message);
     }
 
 }
