23bbb0d039b65354ef987b223c9c2c8003270f41
icecoff22 <icecoff2222@gmail.com>
2024-11-21 21:04:43 +0900

    feat : jpa 의존성 및 base entity 추가

---
+++ meerket/meerket-domain/src/main/java/org/j1p5/domain/global/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package org.j1p5.domain.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories("org.j1p5.domain")
+public class JpaConfig {
+}
