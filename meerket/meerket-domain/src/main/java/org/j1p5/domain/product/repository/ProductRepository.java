e2169083b3959d58f6a3d31b7f1884e26c364be8
sunghyun <20190171@inu.ac.kr>
2024-11-22 17:16:28 +0900

    feat : Product 레포지토리 구현

---
+++ meerket/meerket-domain/src/main/java/org/j1p5/domain/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.j1p5.domain.product.repository;
+
+import org.j1p5.domain.product.entity.ProductEntity;
+import org.j1p5.domain.product.repository.querydsl.ProductRepositoryCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity,Long>, ProductRepositoryCustom {
+}
