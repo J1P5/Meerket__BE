080699ad8c935b1d04bc950651eac8243756a9f6
sunghyun <20190171@inu.ac.kr>
2024-11-22 17:45:06 +0900

    feat : Image 엔티티 구현

---
+++ meerket/meerket-domain/src/main/java/org/j1p5/domain/image/ImageEntity.java
@@ -0,0 +1,24 @@
+package org.j1p5.domain.image;
+
+import jakarta.persistence.*;
+import org.j1p5.domain.global.entity.BaseEntity;
+import org.j1p5.domain.product.entity.ProductEntity;
+
+
+@Entity
+public class ImageEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+
+
+}
