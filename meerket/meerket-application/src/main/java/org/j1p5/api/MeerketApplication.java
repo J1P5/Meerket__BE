c7d371d7912a74a4533e35b46f349c5ee07f6b65
sunghyun <20190171@inu.ac.kr>
2024-11-23 19:21:08 +0900

    chore : 다른 패키지 componentscan 설정

--- meerket/meerket-application/src/main/java/org/j1p5/api/MeerketApplication.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/MeerketApplication.java
@@ -3,10 +3,11 @@ package org.j1p5.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@EntityScan(basePackages = "org.j1p5.domain.product.entity")
+@ComponentScan(basePackages = "org.j1p5")
 public class MeerketApplication {
 
     public static void main(String[] args) {
