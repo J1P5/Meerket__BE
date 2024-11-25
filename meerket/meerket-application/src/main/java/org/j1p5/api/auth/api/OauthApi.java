c452415542a923c96d6582c71b4a615ccb5d74cc
icecoff22 <icecoff2222@gmail.com>
2024-11-26 04:27:24 +0900

    feat : oauth login api 추가

--- meerket/meerket-application/src/main/java/org/j1p5/api/auth/api/OauthApi.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/auth/api/OauthApi.java
@@ -1,2 +1,26 @@
-package org.j1p5.api.auth.api;public class OauthApi {
+package org.j1p5.api.auth.api;
+
+import lombok.RequiredArgsConstructor;
+import org.j1p5.api.auth.dto.LoginRequest;
+import org.j1p5.api.global.response.Response;
+import org.j1p5.domain.auth.OauthService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/oauth")
+public class OauthApi {
+
+    private final OauthService oauthService;
+
+    @PostMapping
+    public Response<Void> login(
+            @RequestBody LoginRequest loginRequest
+    ) {
+        oauthService.login(loginRequest.code(), loginRequest.provider());
+        return Response.onSuccess();
+    }
 }
--- meerket/meerket-application/src/main/java/org/j1p5/api/auth/dto/LoginRequest.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/auth/dto/LoginRequest.java
@@ -1,2 +1,4 @@
-package org.j1p5.api.auth.dto;public class LoginRequest {
+package org.j1p5.api.auth.dto;
+
+public record LoginRequest(String code, String provider) {
 }
