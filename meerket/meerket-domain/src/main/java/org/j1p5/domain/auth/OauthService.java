3e67e5fdf958200af243b43ad7f8a63d025b1c36
icecoff22 <icecoff2222@gmail.com>
2024-11-26 09:50:07 +0900

    refactor : oauth 책임 분리

--- meerket/meerket-domain/src/main/java/org/j1p5/domain/auth/OauthService.java
+++ meerket/meerket-domain/src/main/java/org/j1p5/domain/auth/OauthService.java
@@ -1,32 +1,21 @@
 package org.j1p5.domain.auth;
 
-import org.j1p5.domain.user.entity.Provider;
+import lombok.RequiredArgsConstructor;
 import org.j1p5.domain.user.entity.UserEntity;
 import org.j1p5.domain.user.service.UserAppender;
 import org.j1p5.domain.user.service.UserReader;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 @Service
+@RequiredArgsConstructor
 public class OauthService {
 
+    private final OauthSender oauthSender;
     private final UserReader userReader;
     private final UserAppender userAppender;
-    private final Map<Provider, OauthClient> clients;
-
-    public OauthService(UserReader reader, UserAppender appender, List<OauthClient> oauthClients) {
-        this.userReader = reader;
-        this.userAppender = appender;
-        this.clients = oauthClients.stream().collect(
-                Collectors.toUnmodifiableMap(OauthClient::getProvider, oauthClient -> oauthClient)
-        );
-    }
 
     public Long login(String code, String provider) {
-        OauthProfile profile = request(code, provider);
+        OauthProfile profile = oauthSender.request(code, provider);
 
         UserEntity user = userReader.read(profile.getEmail(), provider);
         if (user == null) {
@@ -36,13 +25,5 @@ public class OauthService {
 
         return user.getId();
 
-    }
-
-    private OauthProfile request(String code, String provider) {
-        OauthClient oauthClient = clients.get(Provider.valueOf(provider));
-
-        OauthToken token = oauthClient.getOauthToken(code);
-
-        return oauthClient.getOauthProfile(token.getAccessToken());
     }
 }
