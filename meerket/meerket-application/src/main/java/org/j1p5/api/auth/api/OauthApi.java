d6559050d7d03fae9ddeca3901ea6524c7f93f68
icecoff22 <icecoff2222@gmail.com>
2024-11-27 15:45:49 +0900

    refactor : spring context session 속에 저장

--- meerket/meerket-application/src/main/java/org/j1p5/api/auth/api/OauthApi.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/auth/api/OauthApi.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.j1p5.api.auth.AuthManager;
 import org.j1p5.api.auth.dto.LoginRequest;
+import org.j1p5.api.global.annotation.LoginUser;
 import org.j1p5.api.global.response.Response;
 import org.j1p5.domain.auth.OauthService;
 import org.j1p5.domain.user.UserInfo;
---
+++ meerket/meerket-application/src/main/java/org/j1p5/api/global/annotation/LoginUserArgumentResolver.java
@@ -0,0 +1,2 @@
+package org.j1p5.api.global.annotation;public class LoginUserArgumentResolver {
+}
