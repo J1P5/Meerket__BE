4631b99b32d080ad721860421e36902e341f25b5
sunghyun <20190171@inu.ac.kr>
2024-12-01 16:48:33 +0900

    product update requestdto 구현

---
+++ meerket/meerket-application/src/main/java/org/j1p5/api/product/dto/request/ProductUpdateRequest.java
@@ -0,0 +1,31 @@
+package org.j1p5.api.product.dto.request;
+
+import org.j1p5.domain.product.dto.ProductUpdateInfo;
+import org.j1p5.domain.product.entity.ProductCategory;
+
+import java.time.LocalDateTime;
+
+public record ProductUpdateRequest(
+        String title,
+        String content,
+        Integer minimumPrice,
+        ProductCategory category,
+        LocalDateTime expiredTime,
+        Double latitude,
+        Double longtitude,
+        String location
+
+) {
+    public static ProductUpdateInfo toInfo(ProductUpdateRequest request){
+        return new ProductUpdateInfo(
+                request.title,
+                request.content,
+                request.minimumPrice,
+                request.category,
+                request.expiredTime,
+                request.latitude,
+                request.longtitude,
+                request.location
+        );
+    }
+}
