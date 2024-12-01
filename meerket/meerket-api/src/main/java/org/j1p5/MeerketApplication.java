b13275e1fb5b17aa66e39650dea4f64bcf12af59
icecoff22 <icecoff2222@gmail.com>
2024-12-02 02:39:39 +0900

    refactor : spring boot 실행 클래스 위치 수정

--- meerket/meerket-api/src/main/java/org/j1p5/MeerketApplication.java
+++ meerket/meerket-api/src/main/java/org/j1p5/MeerketApplication.java
@@ -3,11 +3,8 @@ package org.j1p5.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "org.j1p5")
-@EntityScan(basePackages = "org.j1p5") // 엔티티 스캔
 public class MeerketApplication {
 
     public static void main(String[] args) {
