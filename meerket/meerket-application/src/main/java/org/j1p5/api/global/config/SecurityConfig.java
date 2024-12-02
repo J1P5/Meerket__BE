6814ad1dc6461be8a7049f8e779715e1c861197d
icecoff22 <icecoff2222@gmail.com>
2024-12-02 16:56:12 +0900

    feat : security 401, 403 커스텀 에러 처리 구현

--- meerket/meerket-application/src/main/java/org/j1p5/api/global/config/SecurityConfig.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/global/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package org.j1p5.api.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.j1p5.api.global.handler.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CorsConfig corsConfig;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,6 +34,13 @@ public class SecurityConfig {
                     //
                     // authorize.requestMatchers("/api/v1/admin").hasRole("ADMIN");
                 });
+
+        http.exceptionHandling(
+                e -> {
+                    e.accessDeniedHandler(customAccessDeniedHandler);
+                    e.authenticationEntryPoint(customAuthenticationEntryPoint);
+                }
+        )
 
         http.sessionManagement(
                 session ->
---
+++ meerket/meerket-application/src/main/java/org/j1p5/api/global/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package org.j1p5.api.global.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.j1p5.api.global.response.Response;
+import org.springdoc.api.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        Response<Void> notAuthenticationResponse = Response.onFailure("AUTH401", "인증 정보가 잘못되었습니다.");
+        String responseBody = objectMapper.writeValueAsString(notAuthenticationResponse);
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseBody);
+    }
+}
