964d94ddd379c742a575cc1e814d1eb64a26f58c
icecoff22 <icecoff2222@gmail.com>
2024-12-06 10:52:53 +0900

    feat : 지역 인증 관련 에러코드 추가

--- meerket/meerket-application/src/main/java/org/j1p5/api/areaAuth/exception/PointNotMatchException.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/areaAuth/exception/PointNotMatchException.java
@@ -1,2 +1,10 @@
-package org.j1p5.api.areaAuth.exception;public class PointNotMatchException {
+package org.j1p5.api.areaAuth.exception;
+
+import org.j1p5.api.global.excpetion.WebException;
+import org.j1p5.common.exception.BaseErrorCode;
+
+public class PointNotMatchException extends WebException {
+    public PointNotMatchException(BaseErrorCode baseErrorCode) {
+        super(baseErrorCode);
+    }
 }
