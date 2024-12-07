a6df31dda5414ae927d6a7deacf6793a31c0202a
icecoff22 <icecoff2222@gmail.com>
2024-12-07 20:20:02 +0900

    fix : 첫 로그인(회원가입) 시 500에러 반환 오류 수정

--- meerket/meerket-application/src/main/java/org/j1p5/api/auth/dto/LoginResponse.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/auth/dto/LoginResponse.java
@@ -1,10 +1,17 @@
 package org.j1p5.api.auth.dto;
 
-import org.j1p5.domain.activityArea.dto.SimpleAddress;
 import org.j1p5.domain.user.UserInfo;
 
 public record LoginResponse(String nickname, String profileUrl, Integer emdId, String emdName) {
     public static LoginResponse from(UserInfo userInfo) {
+        if (userInfo.activityAreaInfo() == null) {
+            return new LoginResponse(
+                    userInfo.nickname(),
+                    userInfo.imageUrl(),
+                    null,
+                    null
+            );
+        }
         return new LoginResponse(
                 userInfo.nickname(),
                 userInfo.imageUrl(),
