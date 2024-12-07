107a12ed4dfaeeb22accd549942fcce0d51acb37
icecoff22 <icecoff2222@gmail.com>
2024-12-07 18:06:20 +0900

    fix : octet-stream 컨버터 추가

--- meerket/meerket-application/src/main/java/org/j1p5/api/global/config/WebConfig.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package org.j1p5.api.global.config;
 import java.util.List;
 import org.j1p5.api.global.annotation.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,14 +11,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final CursorArgumentResolver cursorDefaultArgumentResolver;
+    private OctetStreamMsgConverter octetStreamReadMsgConverter;
 
-    public WebConfig(CursorArgumentResolver resolver) {
+    public WebConfig(CursorArgumentResolver resolver, OctetStreamMsgConverter octetStreamReadMsgConverter) {
         this.cursorDefaultArgumentResolver = resolver;
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CursorArgumentResolver());
         resolvers.add(new LoginUserArgumentResolver());
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(octetStreamReadMsgConverter);
     }
 }
