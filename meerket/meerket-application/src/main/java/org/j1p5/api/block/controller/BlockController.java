d2df3428de3a34cbe230aa01098858bf6dc1c551
icecoff22 <icecoff2222@gmail.com>
2024-12-18 01:57:56 +0900

    feat : 차단 사용자 조회 기능 구현

--- meerket/meerket-application/src/main/java/org/j1p5/api/block/controller/BlockController.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/block/controller/BlockController.java
@@ -3,19 +3,20 @@ package org.j1p5.api.block.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.j1p5.api.block.dto.BlockRegisterRequest;
+import org.j1p5.api.block.service.BlockReadService;
 import org.j1p5.api.block.service.BlockRegisterService;
 import org.j1p5.api.global.annotation.LoginUser;
 import org.j1p5.api.global.response.Response;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.j1p5.common.dto.PageResult;
+import org.j1p5.domain.block.BlockUserInfo;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/blocks")
 public class BlockController {
     private final BlockRegisterService blockRegisterService;
+    private final BlockReadService blockReadService;
 
     @Operation(summary = "차단", description = "사용자 차단 등록 API")
     @PostMapping
@@ -25,5 +26,15 @@ public class BlockController {
     ) {
         blockRegisterService.register(userId, blockRegisterRequest.blockId());
         return Response.onSuccess();
+    }
+
+    @Operation(summary = "차단 조회", description = "사용자 차단 조회 API")
+    @GetMapping
+    public Response<PageResult<BlockUserInfo>> readBlocks(
+            @LoginUser Long userId,
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size
+    ) {
+        return Response.onSuccess(blockReadService.read(userId, page, size));
     }
 }
--- meerket/meerket-application/src/main/java/org/j1p5/api/block/service/BlockReadService.java
+++ meerket/meerket-application/src/main/java/org/j1p5/api/block/service/BlockReadService.java
@@ -1,4 +1,42 @@
 package org.j1p5.api.block.service;
 
+import lombok.RequiredArgsConstructor;
+import org.j1p5.api.user.exception.UserNotFoundException;
+import org.j1p5.common.dto.PageResult;
+import org.j1p5.domain.block.BlockUserInfo;
+import org.j1p5.domain.block.entity.BlockEntity;
+import org.j1p5.domain.block.repository.BlockRepository;
+import org.j1p5.domain.user.entity.UserEntity;
+import org.j1p5.domain.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.j1p5.api.global.excpetion.WebErrorCode.USER_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
 public class BlockReadService {
+
+    private final BlockRepository blockRepository;
+    private final UserRepository userRepository;
+
+    public PageResult<BlockUserInfo> read(Long userId, Integer page, Integer size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        UserEntity user = validateUserId(userId);
+        List<BlockEntity> blockedUsers = blockRepository.findByUser(user);
+        List<Long> blockedUserIds = blockedUsers.stream()
+                .map(b -> b.getBlockedUserId().getId()).toList();
+
+        Page<BlockUserInfo> blockUserInfos = userRepository.findBlockUserByIds(blockedUserIds, pageRequest);
+        return PageResult.of(blockUserInfos.getContent(), blockUserInfos.getTotalPages(), blockUserInfos.hasNext());
+    }
+
+    private UserEntity validateUserId(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+    }
 }
