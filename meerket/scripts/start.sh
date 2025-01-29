552bd97458fb3cb322fef5959ae03f1ec11dcc1e
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2025-01-29 13:51:45 +0900

    chore : Java프로퍼티에 환경변수 로드 명령어 추가

--- meerket/scripts/start.sh
+++ meerket/scripts/start.sh
@@ -17,9 +17,28 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+# Java 시스템 프로퍼티로 환경변수 설정
+JAVA_OPTS="-Dhome.url=$HOME_URL \
+-Dhome.username=$HOME_USERNAME \
+-Dhome.password=$HOME_PASSWORD \
+-Dkakao.oauth.grant.type=$KAKAO_OAUTH_GRANT_TYPE \
+-Dkakao.oauth.client.id=$KAKAO_OAUTH_CLIENT_ID \
+-Dkakao.oauth.redirect.uri=$KAKAO_OAUTH_REDIRECT_URI \
+-Dnaver.oauth.grant.type=$NAVER_OAUTH_GRANT_TYPE \
+-Dnaver.oauth.client.id=$NAVER_OAUTH_CLIENT_ID \
+-Dnaver.oauth.client.secret=$NAVER_OAUTH_CLIENT_SECRET \
+-Dnaver.oauth.state=$NAVER_OAUTH_STATE \
+-Daws.access.key.id=$AWS_ACCESS_KEY_ID \
+-Daws.access.key=$AWS_ACCESS_KEY \
+-Daws.s3.bucket.name=$AWS_S3_BUCKET_NAME \
+-Dmongo.database=$MONGO_DATABASE \
+-Dmongo.uri=$MONGO_URI \
+-Dredis.host=$REDIS_HOST \
+-Dredis.port=$REDIS_PORT"
+
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java $JAVA_OPTS -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
