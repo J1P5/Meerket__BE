88a7b83231c476bbcd3b30c20ad41768d854fa6f
icecoff22 <icecoff2222@gmail.com>
2025-04-10 10:56:22 +0900

    feat : 프론트 서버 주소 프로퍼티 추가

---
+++ meerket/meerket-infrastructure/src/main/java/org/j1p5/infrastructure/global/property/FrontServerProperty.java
@@ -0,0 +1,9 @@
+package org.j1p5.infrastructure.global.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("front.server")
+public record FrontServerProperty(
+    String uri
+) {
+}
