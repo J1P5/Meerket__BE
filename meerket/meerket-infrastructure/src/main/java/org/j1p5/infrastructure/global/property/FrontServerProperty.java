dd42569afe3111525e9255e0990388ab3c62a959
icecoff22 <icecoff2222@gmail.com>
2025-04-10 11:00:33 +0900

    refactor : 프론트 서버 프로퍼티 변수명 수정

--- meerket/meerket-infrastructure/src/main/java/org/j1p5/infrastructure/global/property/FrontServerProperty.java
+++ meerket/meerket-infrastructure/src/main/java/org/j1p5/infrastructure/global/property/FrontServerProperty.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("front.server")
 public record FrontServerProperty(
-    String uri
+    String baseUri
 ) {
 }
